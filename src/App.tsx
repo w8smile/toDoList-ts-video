fbde00bf747bf5406068745c2921c27fe6644fba
Serg Nakovnick <w8smile@gmail.com>
2023-12-04 15:08:10 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let tasks1= [
+        {id:1, title: 'CSS', isDone: true},
+        {id:2, title: 'JS', isDone: false},
+        {id:3, title: 'React', isDone: false},
+    ]
+    let tasks2 = [
+        {id:1, title: 'Terminator', isDone: true},
+        {id:2, title: 'RobinGood', isDone: true},
+        {id:3, title: 'Jentlments of fortune', isDone: false},
+    ]
+
+    return (
+
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Movies" tasks={tasks2}/>
+
+
+        </div>
+    );
 }
 
 export default App;
